026156921b1f72ce3607d90bdb480cacd23c7b3d
Zhiyar_Qaradaghi <zhiyar.burhan2003@gmail.com>
2025-01-24 03:54:00 +0300

    Hibernate Object Equity

--- spring-6-webapp-main/src/main/java/guru/springframework/spring6webapp/domain/Author.java
+++ spring-6-webapp-main/src/main/java/guru/springframework/spring6webapp/domain/Author.java
@@ -12,8 +12,14 @@ public class Author {
     private String firstName;
     private String lastName;
 
+
+    // When working with JPA entities - set an equals and hashCode method because hibernate is going to use this internally to determine object equity
+    // YOu can just use the id property - if 2 entities have the same ID then hibernate considers them equal entities
+    // YOu can use the variables/properties of the entire class instead of just the id
+
     @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
+
 
     public Set<Book> getBooks() {
         return books;
@@ -45,5 +51,32 @@ public class Author {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    // equals and hashcode for hibernate to determine object equity -- basically for hibernate to tell if 2 objects are the same or not
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if(!(o instanceof Author)) {
+            return false;
+        }
+        Author author = (Author) o;
+
+        return getId() != null ? getId().equals(author.getId()) : author.getId() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() !=null ? getId().hashCode() : 0;
     }
 }
